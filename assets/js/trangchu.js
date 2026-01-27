84e79c89ed98497f48acd4f76855dfde032f2666
letruces <lenhatruc12345@gmail.com>
2026-01-27 10:18:32 +0700

    Xong cơ bẩn phần trang chủ

---
+++ assets/js/trangchu.js
@@ -0,0 +1,26 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const btn = item.querySelector(".faq-question");
+  const answer = item.querySelector(".faq-answer");
+
+  btn.addEventListener("click", () => {
+    // đóng item khác
+    faqItems.forEach((i) => {
+      if (i !== item) {
+        i.classList.remove("active");
+        const otherAnswer = i.querySelector(".faq-answer");
+        otherAnswer.style.height = 0;
+      }
+    });
+
+    // toggle current
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+      answer.style.height = 0;
+    } else {
+      item.classList.add("active");
+      answer.style.height = answer.scrollHeight + "px";
+    }
+  });
+});
