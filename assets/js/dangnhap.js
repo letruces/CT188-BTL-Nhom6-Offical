0780fdc86a41ed3f26dd1112634a916c77b5ecbc
letruces <lenhatruc12345@gmail.com>
2026-01-29 23:33:11 +0700

    first commit

---
+++ assets/js/dangnhap.js
@@ -0,0 +1,19 @@
+function login() {
+  const correctUser = "admin";
+  const correctPass = "123456";
+
+  const username = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  if (username === "" || password === "") {
+    alert("Nhập đầy đủ thông tin!");
+  } else if (username === correctUser && password === correctPass) {
+    window.location.href = "trangchu.html";
+  } else {
+    alert("Sai tài khoản hoặc mật khẩu!");
+  }
+}
+
+function togglePassword() {
+  const passwordInput = document.getElementById("password");
+  passwordInput.type === "password" ? "text" : "password";
+}
