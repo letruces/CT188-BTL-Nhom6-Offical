3475a51de67661f5f3de2ea4f2d517e878e59e05
letruces <lenhatruc12345@gmail.com>
2026-02-25 17:06:41 +0700

    done responsive khóa học

--- assets/js/khoahocrieng.js
+++ assets/js/khoahocrieng.js
@@ -32,3 +32,28 @@ moreBtn.addEventListener("click", function () {
   teacherMore.classList.toggle("active");
   moreBtn.classList.toggle("active");
 });
+// Scroll Spy Sidebar
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          if (link.getAttribute("href").substring(1) === entry.target.id) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.8, // 80% section xuất hiện mới active
+  },
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
